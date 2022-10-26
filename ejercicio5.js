dd1519d9865ebeca9a49d5367614864bb0a6756d
danieladbsnt <daniela.darnea@bosonit.com>
2022-10-26 13:19:32 +0200

    added ex 5

---
+++ ejercicio5.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*Dado un array de ciudades, sacar en un nuevo array las ciudades no capitales con unos nuevos parámetros que sean city y isSpain. 
+El valor de isSpain será un booleano indicando si es una ciudad de España. Ejemplo: {city: "Logroño", isSpain: "true"}*/
+
+const arrCities2 = [
+    {city: 'Logroño', country: 'Spain', capital: false},
+    {city: 'Bordeaux', country: 'France', capital: false},
+    {city: 'Madrid', country: 'Spain', capital: true},
+    {city: 'Florence', country: 'Italy', capital: true},
+    {city: 'Oslo', country: 'Norway', capital: true},
+    {city: 'Jaén', country: 'Spain', capital: false}
+  ]
+
+const arrNoCapitales = arrCities2.filter(arrCity => {
+    if (!arrCity.capital) return arrCity;
+})
+
+console.log('no capitales: ', arrNoCapitales);
+
+const newArrCities = arrNoCapitales.map(elmt => {
+    elmt.country === 'Spain' ? elmt.isSpain = true : elmt.isSpain = false;
+    return elmt;
+});
+    
+console.log('Ciudades: ', newArrCities);
